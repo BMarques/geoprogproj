1b8174e7a45c8a4ab564a14a808fd9d724ea5476
Bruno Marques <brunormarques@live.com.pt>
2023-01-07 15:57:50 +0000

    Final version of the project.

--- project.py
+++ project.py
@@ -2,49 +2,36 @@ import os
 
 # PostGIS sets GDAL_DATA and PROJ_LIB on the system environments to its own folders, and due to that fiona stopped working properly.
 # The next two lines will remove those variables from the environment dictionary. 
-# This is not documented anywhere, it's my own solution
 os.environ.pop('GDAL_DATA', None)
 os.environ.pop('PROJ_LIB', None)
 
-import shutil
-import fiona
-from fiona.crs import from_epsg
-from pyproj import Transformer, CRS
-import urllib.request
-import geojson
-from geojson import FeatureCollection
-from shapely.geometry import shape, mapping
-from shapely.ops import unary_union
 import itertools
+import shutil
+import urllib.request
+import warnings
+from datetime import datetime
 from operator import itemgetter
 
+import fiona
+import geojson
 import psycopg2
 import psycopg2.errors
-
-from osgeo import gdal
-from osgeo import ogr
-from osgeo import osr
-
+from fiona.crs import from_epsg
+from osgeo import gdal, ogr, osr
+from pyproj import Transformer
 from rasterstats import zonal_stats
+from shapely.errors import ShapelyDeprecationWarning
+from shapely.geometry import mapping, shape
+from shapely.ops import unary_union
 
-# General workflow
-
-# Create output and temporary folders
-# Reproject CAOP into WGS 84, which is the coordinate system of ipma's data
-# Dissolve CAOP into counties
-# Obtain IPMA's data
-# Save to the database
-# Remove stations data outside the boundary of Portugal Continental
-# Interpolate to get data on counties with no data
-# Create a raster of the interpolation and clip it to the boundary of Portugal Continental
-# Create a choropleth map
 
 def create_folders(paths):
-    # Always recreate the folders, even if it existed previously
     for path in paths:
         if os.path.exists(path):
-            shutil.rmtree(path)
-        os.mkdir(path)
+            return
+
+        # function makedirs allows to create folders and subfolders, unlike mkdir
+        os.makedirs(path)
         print("\t Folder created: {0}".format(path))
     
 def delete_folders(paths):
@@ -52,7 +39,7 @@ def delete_folders(paths):
         if os.path.exists(path):
             shutil.rmtree(path)
 
-def reproject(infile, outfile, epsg):
+def reproject(infile, outfile, epsg, swap_coordinates = False):
     # Only reproject if the reprojected file does not exist.
     # This is an exception because it's still an input data, not an output and will probably never change.
     # As such, speeds up the processing time.
@@ -62,27 +49,25 @@ def reproject(infile, outfile, epsg):
 
     # The reprojection works by creating a new output file with the new coordinate system.
     # Then every coordinate needs to be transformed from the input coordinate system to the output coordinate system.
-    try:
-        print("\t Reprojecting {0} to EPSG {1}...".format(infile, epsg))
-        with fiona.open(infile) as input:
-            output_schema = input.schema.copy()
-            with fiona.open(outfile, 'w', input.driver, output_schema, from_epsg(epsg)) as output:
-                transformer = Transformer.from_crs(input.crs_wkt, output.crs_wkt)
-                for feature in input:
-                    new_coords = []
-                    for coordinate in feature['geometry']['coordinates']:
-                        x2, y2 = transformer.transform(*zip(*coordinate)) # Swap the coordinates. EPSG 4326 uses lon / lat, not lat / lon
+    print("\t Reprojecting {0} to EPSG {1}...".format(infile, epsg))
+    with fiona.open(infile) as input:
+        with fiona.open(outfile, 'w', input.driver, input.schema, from_epsg(epsg)) as output:
+            transformer = Transformer.from_crs(input.crs_wkt, output.crs_wkt)
+            for feature in input:
+                new_coords = []
+                for coordinate in feature['geometry']['coordinates']:
+                    x2, y2 = transformer.transform(*zip(*coordinate))
+                    if (swap_coordinates):
                         new_coords.append(zip(y2, x2))
-                    feature['geometry']['coordinates'] = new_coords
-                    output.write(feature)
-    except:
-        print("\t Reprojection failed, cleanup any created files.")
+                    else:
+                        new_coords.append(zip(x2, y2))
+                feature['geometry']['coordinates'] = new_coords
+                output.write(feature)
 
-# Implementation of the dissolve inspired by the dissolve in ArcGIS
+# Implementation of the dissolve inspired by the dissolve geoprocessing tool in ArcGIS
 # If no fields are specified, it will dissolve every geometry. Useful to have a country boundary.
 # Else it will dissolve by the parametres specified
 # This solution was inspired by an answer on the GIS StackExchange: https://gis.stackexchange.com/a/274516
-# It will group every item in 
 def dissolve(infile, outfile, fields=[], print_tab_count = 1):
     print('{0} Start dissolve geoprocessing on {1}...'.format('\t' * print_tab_count, infile))
     with fiona.open(infile) as input:
@@ -109,9 +94,15 @@ def save_observation_data(data):
                 geom = geojson.dumps(feature.geometry)
                 properties = feature.properties
                 try:
-                    cur.execute("""INSERT INTO main.stationdata (geom, intensidadeVentoKM, temperatura, idEstacao, pressao, humidade, localEstacao, precAcumulada, idDireccVento, radiacao, time, intensidadeVento, descDirVento)
+                    cur.execute("""INSERT INTO main.stationdata 
+                                (geom, intensidadeVentoKM, temperatura, idEstacao, pressao,
+                                 humidade, localEstacao, precAcumulada, idDireccVento, radiacao,
+                                 time, intensidadeVento, descDirVento)
                                 VALUES (ST_GeomFromGeoJSON(%s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
-                                , (geom, properties['intensidadeVentoKM'], properties['temperatura'], properties['idEstacao'], properties['pressao'], properties['humidade'], properties['localEstacao'],properties['precAcumulada'],properties['idDireccVento'],properties['radiacao'],properties['time'], properties['intensidadeVento'], properties['descDirVento']))
+                                , (geom, properties['intensidadeVentoKM'], properties['temperatura'], properties['idEstacao'],
+                                 properties['pressao'], properties['humidade'], properties['localEstacao'],properties['precAcumulada'],
+                                 properties['idDireccVento'],properties['radiacao'],properties['time'], properties['intensidadeVento'],
+                                  properties['descDirVento']))
                 except psycopg2.errors.UniqueViolation:
                     # Ignore this error, happens many times in tests. 
                     # Should never happen with IPMA's own data because the script should run every X hours on a schedule.
@@ -122,11 +113,6 @@ def save_observation_data(data):
                     conn.commit()            
 
 def import_ipma_data():
-    # This function fully utilizes GDAL instead of fiona and shapely
-    # as it is very efficient to clip station data and will be used to generate
-    # the interpolation and then also to clip the interpolation raster.
-    # GDAL itself was already installed by fiona, so it's not an extra library.
-
     print("\t Importing data from the last 3 hours from IPMA's API ...")
     
     # First obtain the data
@@ -138,15 +124,20 @@ def import_ipma_data():
     print ("\t Saving data to the database ...")
     save_observation_data(observation_data)
 
-    print("\t Import finished")
+    print("\t Import finished.")
 
-def interpolate_temperature(shapefile, output_path, tmp_path):
+def interpolate_temperature(counties_shpfile, stations_shapefile, output_path, tmp_path):
+    
+    # This function fully utilizes GDAL instead of fiona and shapely
+    # as it is very efficient to clip station data and will be used to generate
+    # the interpolation and then also to clip the interpolation raster.
+    # GDAL itself was already installed by fiona, so it's not an extra library.
 
     print ("\t Start interpolation process:")
     # First, let's create a boundary of Portugal Continental
     print("\t\t Creating boundary of Portugal Continental...")
     boundary_file = os.path.join(output_path, "boundary_portugal_continental.shp")
-    dissolve(shapefile, boundary_file, print_tab_count = 2)
+    dissolve(counties_shpfile, boundary_file, print_tab_count = 2)
     
     driver = ogr.GetDriverByName("ESRI Shapefile")
     dataSource = driver.Open(boundary_file, 0)
@@ -174,7 +165,6 @@ def interpolate_temperature(shapefile, output_path, tmp_path):
     lyr.SetSpatialFilter(boundary)
 
     print ("\t\t Generating the shapefile that has the stations data. Needed for the interpolation...")
-    stations_shapefile = os.path.join(output_path, 'stationpoints.shp')
 
     # Generate the point shapefile 
     data_source = driver.CreateDataSource(stations_shapefile)
@@ -184,24 +174,24 @@ def interpolate_temperature(shapefile, output_path, tmp_path):
     srs.ImportFromEPSG(4326)
 
     # create the layer
-    layer = data_source.CreateLayer('points', srs, ogr.wkbPoint)
+    new_lyr = data_source.CreateLayer('points', srs, ogr.wkbPoint)
 
     # Add the fields we're interested in
-    layer.CreateField(ogr.FieldDefn('Temp', ogr.OFTReal))
+    new_lyr.CreateField(ogr.FieldDefn('Temp', ogr.OFTReal))
     field_local = ogr.FieldDefn('Local', ogr.OFTString)
     field_local.SetWidth(80)
-    layer.CreateField(field_local)
+    new_lyr.CreateField(field_local)
 
     for lyr_feature in lyr:
         # create the feature
-        feature = ogr.Feature(layer.GetLayerDefn())
+        feature = ogr.Feature(new_lyr.GetLayerDefn())
         # Set the attributes using the values from the postgis layer
         feature.SetField('Temp', lyr_feature.GetField('temperatura'))
         feature.SetField('Local', lyr_feature.GetField('localestacao'))
 
         feature.SetGeometry(lyr_feature.GetGeometryRef())
 
-        layer.CreateFeature(feature)
+        new_lyr.CreateFeature(feature)
 
         feature = None
 
@@ -216,7 +206,8 @@ def interpolate_temperature(shapefile, output_path, tmp_path):
     
     # Create an interpolation with IDW and save an uncliped file to the tmp folder.
     idw_file = os.path.join(tmp_path, "invdist.tif") 
-    gdal.Grid(idw_file, stations_shapefile, zfield="Temp", algorithm = "invdist", outputBounds=bounds_corrected, width=800, height = 800)
+    gdal.Grid(idw_file, stations_shapefile, zfield="Temp", algorithm = "invdistnn",
+              outputBounds=bounds_corrected, width = 800, height = 800)
 
     print ("\t\t Clipping raster to the boundary of Portugal Continental...")
     idw_clipped_file = os.path.join(output_path, "invdist_clip.tif")
@@ -228,7 +219,11 @@ def interpolate_temperature(shapefile, output_path, tmp_path):
     return idw_clipped_file
 
 def generate_zonal_stats(shapefile, idw, output_path):
-    print ("Calculate zonal stats to get a mean value of the temperature in every county.")
+    # Warning fired by Shapely, because of the rasterstats library.
+    # It's a known issue: https://github.com/perrygeo/python-rasterstats/issues/270
+    warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning) 
+
+    print ("\t Calculating zonal statistics to get a mean value of the temperature in every county...")
     # Calculate zonal statistics to get the mean temperature in every Country
     stats = zonal_stats(shapefile, idw, stats="mean", geojson_out = True, nodata = -999)
 
@@ -249,33 +244,38 @@ def generate_zonal_stats(shapefile, idw, output_path):
                 }
             }
             output.write(feature)
+    print ("\t Zonal Statistics calculation finished. Shapefile with the average temperatures per county generated.")
+
     
 def start_processing():
     BASE_PATH = os.getcwd()
     DATA_PATH = os.path.join(BASE_PATH, "dados")
     OUTPUT_PATH = os.path.join(BASE_PATH, "output")
+    SESSION_PATH = os.path.join(OUTPUT_PATH, datetime.now().strftime("%m-%d-%Y-%H%M%S"))
     TMP_PATH = os.path.join(BASE_PATH, "tmp")
     CAOP_FILE = os.path.join(DATA_PATH, 'Cont_AAD_CAOP2021', 'Cont_AAD_CAOP2021.shp')
     REPROJECTED_CAOP_FILE = os.path.join(DATA_PATH, 'Cont_AAD_CAOP2021', 'Cont_AAD_CAOP2021_Reprojected.shp')
-    COUNTY_CAOP_FILE = os.path.join(OUTPUT_PATH, 'Cont_AAD_CAOP2021_Concelhos.shp')
+    COUNTY_CAOP_FILE = os.path.join(SESSION_PATH, 'Cont_AAD_CAOP2021_Concelhos.shp')
+    STATIONS_FILE = os.path.join(SESSION_PATH, 'stationpoints.shp')
 
-    # First create or recreate the output folder to store the relevant processed data
+    # First create the output and session folders to store the relevant processed data
     # Then create a temporary folder to store files that are needed for intermediate processing
-    create_folders([OUTPUT_PATH, TMP_PATH])
+    create_folders([SESSION_PATH, TMP_PATH])
 
     # This script prefers WGS 84, so first the CAOP needs to be reprojected from EPSG 3763 to 4326
-    reproject(CAOP_FILE, REPROJECTED_CAOP_FILE, 4326)
+    reproject(CAOP_FILE, REPROJECTED_CAOP_FILE, 4326, swap_coordinates = True)
 
-    # Dissolve CAOP, remove every parish (Freguesia)
+    # Dissolve CAOP, remove every parish (Freguesia) and group then by County (Concelho)
     dissolve(REPROJECTED_CAOP_FILE, COUNTY_CAOP_FILE, ['Concelho'])
 
-    # Retrieve the data stations data from IPMA
+    # Retrieve the stations data from IPMA and save it on the database
     import_ipma_data()
 
     # Interpolate temperature
-    temp_idw_raster = interpolate_temperature(COUNTY_CAOP_FILE, OUTPUT_PATH, TMP_PATH)
+    temp_idw_raster = interpolate_temperature(COUNTY_CAOP_FILE, STATIONS_FILE, SESSION_PATH, TMP_PATH)
 
-    generate_zonal_stats(COUNTY_CAOP_FILE, temp_idw_raster, OUTPUT_PATH)
+    # Generate zonal statistics to know the mean value of the temperatures in each country
+    generate_zonal_stats(COUNTY_CAOP_FILE, temp_idw_raster, SESSION_PATH)
 
     # Delete the tmp folder
     delete_folders([TMP_PATH])
